8badfea428366e697277f49a47019577aaba5d7f
Sebastian Waisbrot <seppo0010@gmail.com>
2021-09-25 15:09:52 -0300

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0b90fc94284e2e61c36e";
+/******/ 	var hotCurrentHash = "81274e96f42e99e03205";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
